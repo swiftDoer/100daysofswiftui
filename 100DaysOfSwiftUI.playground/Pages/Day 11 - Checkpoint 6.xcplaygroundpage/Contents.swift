d7710af5ca95de0ccf1b4a9e9c2d88573b0d472e
swiftDoer <94198052+lkdnss@users.noreply.github.com>
2022-09-02 21:10:19 +0200

    Add Day 11 Checkpoint 6

---
+++ 100DaysOfSwiftUI.playground/Pages/Day 11 - Checkpoint 6.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+
+struct Car {
+    private let model: String
+    private let seats: Int
+    private(set) var currentGear = 1
+    static var example = Car(model: "Some Audi", seats: 5)
+    
+    
+    mutating func changeGear(gearUp:Bool) {
+        if currentGear >= 10 && gearUp{
+            print("You can't go over gear 10")
+        } else if currentGear <= 1 && !gearUp{
+            print("You can't go below gear 1")
+        } else if gearUp {
+            currentGear += 1
+        } else {
+            currentGear -= 1
+        }
+    }
+}
+
+
+print(Car.example)
+Car.example.changeGear(gearUp: true)
+print(Car.example)
