95390a75980e297fe831b087853df79b34d9a71e
swiftDoer <94198052+lkdnss@users.noreply.github.com>
2022-09-09 10:27:28 +0200

    small fix

---
+++ WeSplit/WeSplit/Project2Prep.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct Project2Prep:View {
+    var body: some View {
+        Text("Hi!")
+    }
+}
+
+
+struct Project2Prep_Previews: PreviewProvider {
+    static var previews: some View {
+        Project2Prep()
+    }
+}
--- WeSplit/WeSplit/WeSplitApp.swift
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -1,17 +1,10 @@
-//
-//  WeSplitApp.swift
-//  WeSplit
-//
-//  Created by Andreas Nuss on 07.09.22.
-//
-
 import SwiftUI
 
 @main
 struct WeSplitApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView4()
+            Project2Prep()
         }
     }
 }
