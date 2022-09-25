e24258f15be95838c145a240ed931570c9d5255d
swiftDoer <94198052+lkdnss@users.noreply.github.com>
2022-09-25 17:45:06 +0200

    add shisan

---
+++ Drawing/Drawing/useless2/ContentView.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            VStack {
+                
+                Text("_       _")
+                Text(" '      |   /    ` ")
+                Text("草草")
+                Text("死死死草死死死")
+                Text("死死死死死死死死死死死")
+                Text("死死死死死死死死死死死死死")
+                Text("死死死                                          死")
+                Text("死                                                       死")
+            }
+            HStack {
+             Text("c@——@")
+                    .font(.system(size: 80))
+            }
+            Text(".         +                  w                .")
+                .font(.headline)
+            Text("                 _________                ")
+            Text("        ----- /                    \\ -----  b:  ")
+            Text("//        /      　死         \\          ")
+            Text("//        /          死           \\          ")
+            Text("◯     /          死死          \\       ")
+        }
+    }
+}
